528927b2b3905749aace381be2868d51ac6a80b0
Anna Kilikova <103356373+klikli19@users.noreply.github.com>
2025-03-26 14:40:14 +0300

    Именованные запросы [#505063]

--- src/main/java/ru/job4j/socialmediaapi/repository/PostRepository.java
+++ src/main/java/ru/job4j/socialmediaapi/repository/PostRepository.java
@@ -1,8 +1,19 @@
 package ru.job4j.socialmediaapi.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import org.springframework.data.repository.CrudRepository;
 import ru.job4j.socialmediaapi.model.Post;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface PostRepository extends CrudRepository<Post, Long> {
+    List<Post> findByUserId(Long id);
+
+    List<Post> findByCreatedIsGreaterThanEqualAndCreatedIsLessThanEqual(LocalDateTime startAt, LocalDateTime endAt);
+
+    Page<Post> findByOrderByCreatedDesc(Pageable pageable);
 
 }
